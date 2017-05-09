e991ca0d7b1c99b14e6e9a90df4e46b1120cfb01
fogray <forget871205@126.com>
2017-05-09 15:22:34 +0800

    aa

--- skins/js/router.js
+++ skins/js/router.js
@@ -1,9 +1,9 @@
 var Router = {
   href: function(url){
-    window.location.href = url;
+    window.location.href = DC_CONFIG.WEBUI_CONTEXT + '/'+url;
   },
   open: function(url, title) {
-    window.open(url, title);
+    window.open(DC_CONFIG.WEBUI_CONTEXT + '/'+url, title);
   },
   reload: function(force) {
     window.location.reload();
